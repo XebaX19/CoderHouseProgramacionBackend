9dce54fefe63b77659b8c14f0b200434aa8aeabc
XebaX19 <seba.bellesi@gmail.com>
2022-05-14 15:46:33 -0300

    Quita desafio clase 34

--- DesafioClase34/middlewares/loggerMiddleware.js
+++
@@ -1,9 +0,0 @@
-const logger = require('../logger/index');
-
-const loggerMiddleware =  (req, res, next) => {
-    logger.info(`[${req.method}] => ${req.originalUrl}`);
-
-    next();//para que se ejecute la siguiente instrucción
-};
-
-module.exports = loggerMiddleware;
